3c5cbd537177f44adf7c1456609606c4e5ee1005
vietudu <Vijayendrav19@gmail.com>
2025-11-14 14:37:46 +0530

    Add files via upload

---
+++ page.jsx
@@ -0,0 +1,1 @@
+export default function Home(){return(<div className='p-10 text-center'><h1 className='text-4xl font-bold'>ScaleXsites</h1><p className='mt-4 text-gray-600'>Your official Next.js website starter.</p></div>);}
